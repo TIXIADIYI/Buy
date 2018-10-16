2f876ac0afb97c82bae06742a7079f18b0659ce5
S6203-1-08 <111>
2018-10-16 15:49:59 +0800

    数据库更新

--- data/buy_data.sql
+++ data/buy_data.sql
@@ -1,113 +1,116 @@
 /*
- Navicat MySQL Data Transfer
+Navicat MySQL Data Transfer
 
- Source Server         : localhost_3306
- Source Server Type    : MySQL
- Source Server Version : 80011
- Source Host           : localhost:3306
- Source Schema         : buy_data
+Source Server         : localhost_3306
+Source Server Version : 60011
+Source Host           : localhost:3306
+Source Database       : buy_data
 
- Target Server Type    : MySQL
- Target Server Version : 80011
- File Encoding         : 65001
+Target Server Type    : MYSQL
+Target Server Version : 60011
+File Encoding         : 65001
 
- Date: 15/10/2018 22:39:50
+Date: 2018-10-16 15:49:22
 */
 
-SET NAMES utf8mb4;
-SET FOREIGN_KEY_CHECKS = 0;
+SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for admins
+-- Table structure for `admins`
 -- ----------------------------
 DROP TABLE IF EXISTS `admins`;
-CREATE TABLE `admins`  (
+CREATE TABLE `admins` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
-  `name` char(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `pass` char(20) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `name` char(10) DEFAULT NULL,
+  `pass` char(20) DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Records of admins
 -- ----------------------------
-INSERT INTO `admins` VALUES (1, 'admin', '123456');
+INSERT INTO `admins` VALUES ('1', 'admin', '123456');
 
 -- ----------------------------
--- Table structure for product_comment
--- ----------------------------
-DROP TABLE IF EXISTS `product_comment`;
-CREATE TABLE `product_comment`  (
-  `comment` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
-  `user_id` int(20) NULL DEFAULT NULL,
-  `product_id` int(20) NULL DEFAULT NULL,
-  `time` date NULL DEFAULT NULL,
-  INDEX `user_id`(`user_id`) USING BTREE,
-  INDEX `product_id`(`product_id`) USING BTREE,
-  CONSTRAINT `product_comment_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE SET NULL ON UPDATE SET NULL,
-  CONSTRAINT `product_comment_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE SET NULL ON UPDATE SET NULL
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Table structure for product_type
--- ----------------------------
-DROP TABLE IF EXISTS `product_type`;
-CREATE TABLE `product_type`  (
-  `id` tinyint(4) NOT NULL AUTO_INCREMENT,
-  `name` char(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  PRIMARY KEY (`id`) USING BTREE,
-  INDEX `t_type`(`name`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of product_type
--- ----------------------------
-INSERT INTO `product_type` VALUES (11, '小吃快餐');
-INSERT INTO `product_type` VALUES (13, '新鲜果蔬');
-INSERT INTO `product_type` VALUES (12, '烧烤烤肉');
-INSERT INTO `product_type` VALUES (10, '甜点饮品');
-INSERT INTO `product_type` VALUES (14, '美味火锅');
-
--- ----------------------------
--- Table structure for products
+-- Table structure for `products`
 -- ----------------------------
 DROP TABLE IF EXISTS `products`;
-CREATE TABLE `products`  (
+CREATE TABLE `products` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
-  `name` char(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `price` float(20, 0) NULL DEFAULT NULL,
-  `remake` text CHARACTER SET utf8 COLLATE utf8_general_ci NULL,
-  `image` char(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `sum` int(50) NULL DEFAULT NULL,
-  `time` date NULL DEFAULT NULL,
-  `click` int(20) NULL DEFAULT NULL,
-  `type` tinyint(20) NULL DEFAULT NULL,
+  `name` char(50) NOT NULL,
+  `price` float(20,0) DEFAULT NULL,
+  `remake` text,
+  `image` char(100) DEFAULT NULL,
+  `sum` int(50) DEFAULT NULL,
+  `time` date DEFAULT NULL,
+  `click` int(20) DEFAULT NULL,
+  `product_type_id` tinyint(20) DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
-  INDEX `p_type`(`type`) USING BTREE,
-  CONSTRAINT `products_ibfk_2` FOREIGN KEY (`type`) REFERENCES `product_type` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+  KEY `p_type` (`product_type_id`) USING BTREE,
+  CONSTRAINT `products_ibfk_1` FOREIGN KEY (`product_type_id`) REFERENCES `product_type` (`id`) ON DELETE SET NULL ON UPDATE SET NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Records of products
 -- ----------------------------
-INSERT INTO `products` VALUES (1, '一个测试的二手商品', NULL, NULL, NULL, NULL, NULL, NULL, NULL);
+INSERT INTO `products` VALUES ('1', '一个测试的二手商品', null, null, null, null, null, null, '1');
+INSERT INTO `products` VALUES ('2', '楠神的商品', null, null, null, null, null, null, '1');
 
 -- ----------------------------
--- Table structure for users
+-- Table structure for `product_comment`
+-- ----------------------------
+DROP TABLE IF EXISTS `product_comment`;
+CREATE TABLE `product_comment` (
+  `comment` text,
+  `user_id` int(20) DEFAULT NULL,
+  `product_id` int(20) DEFAULT NULL,
+  `time` date DEFAULT NULL,
+  `y` int(20) DEFAULT NULL,
+  `n` int(20) DEFAULT NULL,
+  `praise` bit(1) DEFAULT NULL,
+  KEY `user_id` (`user_id`) USING BTREE,
+  KEY `product_id` (`product_id`) USING BTREE,
+  CONSTRAINT `product_comment_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `product_comment_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+-- ----------------------------
+-- Records of product_comment
+-- ----------------------------
+INSERT INTO `product_comment` VALUES ('这个手机很赞！', '2', '1', '2018-10-16', null, null, '');
+
+-- ----------------------------
+-- Table structure for `product_type`
+-- ----------------------------
+DROP TABLE IF EXISTS `product_type`;
+CREATE TABLE `product_type` (
+  `id` tinyint(4) NOT NULL AUTO_INCREMENT,
+  `name` char(50) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `t_type` (`name`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
+
+-- ----------------------------
+-- Records of product_type
+-- ----------------------------
+INSERT INTO `product_type` VALUES ('1', '手机');
+INSERT INTO `product_type` VALUES ('2', '电脑');
+
+-- ----------------------------
+-- Table structure for `users`
 -- ----------------------------
 DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users`  (
+CREATE TABLE `users` (
   `id` int(20) NOT NULL AUTO_INCREMENT,
-  `user` char(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `pass` char(30) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-  `name` char(30) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
-  `header` char(200) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `user` char(30) NOT NULL,
+  `pass` char(30) NOT NULL,
+  `name` char(30) DEFAULT NULL,
+  `header` char(200) DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 ROW_FORMAT=DYNAMIC;
 
 -- ----------------------------
 -- Records of users
 -- ----------------------------
-INSERT INTO `users` VALUES (1, 'user', '123456', '小王', NULL);
-
-SET FOREIGN_KEY_CHECKS = 1;
+INSERT INTO `users` VALUES ('1', 'user', '123456', '小王', null);
+INSERT INTO `users` VALUES ('2', '12345678900', '123456', '楠神', null);
